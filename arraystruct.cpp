01f6a72f64927c0aab2266f604916fdba1a8a062
canzonadzka <canzonadzka@gmail.com>
2026-04-06 09:38:26 +0700

    mengisi array

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -10,4 +10,8 @@ int main()
 
     // cara akses (membaca atau mengambil isi array)
     cout << "Isi data kedua : " << dataku[1] << endl;
+
+    // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
 }
